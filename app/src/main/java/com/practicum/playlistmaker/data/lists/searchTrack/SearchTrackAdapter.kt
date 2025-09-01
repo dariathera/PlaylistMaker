5ec372a3724a57ecd48a60b17c629d1be7ca6f83
Dasha <popovadariaa1111@gmail.com>
2025-09-01 22:17:08 +0300

    Внесла исправления в адаптер. Теперь программа корректно отображает длинные списки (когда все элементы не помещаются на экране).

--- app/src/main/java/com/practicum/playlistmaker/data/lists/searchTrack/SearchTrackAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/lists/searchTrack/SearchTrackAdapter.kt
@@ -2,11 +2,8 @@ package com.practicum.playlistmaker.data.lists.searchTrack
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.view.ViewTreeObserver
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.view.marginLeft
-import androidx.core.view.marginRight
 import androidx.recyclerview.widget.RecyclerView
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.data.objects.Track
@@ -14,8 +11,6 @@ import com.practicum.playlistmaker.data.objects.Track
 class SearchTrackAdapter (
     private val tracks: List<Track>
 ) : RecyclerView.Adapter<SearchTrackViewHolder> () {
-
-    var setArtistNameListener: ViewTreeObserver.OnGlobalLayoutListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchTrackViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.search_track_list_item, parent, false)
@@ -25,33 +20,29 @@ class SearchTrackAdapter (
     override fun onBindViewHolder(holder: SearchTrackViewHolder, position: Int) {
         holder.bind(tracks[position])
 
-        val artistName: TextView = holder.itemView.findViewById(R.id.artistName)
+        holder.itemView.post {
+            val artistName: TextView = holder.itemView.findViewById(R.id.artistName)
 
-        setArtistNameListener = object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                artistName.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                val textViewWidth = artistName.width
-                val textWidth = artistName.paint.measureText(artistName.text.toString())
-                if (textViewWidth > textWidth) {
-                    artistName.layoutParams = LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.WRAP_CONTENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT,
-                        0f
-                    )
-                    tracks[holder.adapterPosition].isFixed = true
-                    holder.itemView.post {
-                        notifyItemChanged(position)
-                    }
-                }
+            val textViewWidth = artistName.width
+            val textWidth = artistName.paint.measureText(artistName.text.toString())
+            if (textViewWidth > textWidth) {
+                artistName.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    0f
+                )
+                tracks[holder.adapterPosition].isFixed = true
             }
         }
-        artistName.viewTreeObserver.addOnGlobalLayoutListener(setArtistNameListener)
-
-
     }
 
     override fun getItemCount(): Int {
         return tracks.size
+    }
+
+    override fun onViewRecycled(holder: SearchTrackViewHolder) {
+        super.onViewRecycled(holder)
+        tracks[holder.adapterPosition].isFixed = false
     }
 
 }
